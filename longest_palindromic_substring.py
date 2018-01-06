a07fda650dd811e01c7aeb3fd53a85bddd12afe8
yao <344098585@qq.com>
2018-01-06 10:21:09 +0800

    longest_palindromic_substring.py

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s_len = len(s)
+        if s_len == 1:
+            return s
+        container = [0, '']
+
+        def getPalindrome(index):
+            for left, right in [(index, index), (index, index + 1)]:
+                print left, right
+                while True:
+                    if left < 0 or right >= s_len:
+                        break
+                    if s[left] != s[right]:
+                        break
+                    else:
+                        length = right - left + 1
+                        if length > container[0]:
+                            container[0], container[1] = length, s[left: right + 1]
+                    left -= 1
+                    right += 1
+
+        for i in range(s_len - 1):
+            getPalindrome(i)
+        return container[1]
